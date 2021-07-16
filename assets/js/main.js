b577ad998e32b7447120c836e77834dbe3c578ea
Rushikesh Kamalapurkar <rushikesh.kamalapurkar@okstate.edu>
2021-07-16 15:29:39 -0500

    aa

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,12 +21,10 @@
 			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+			// $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 1000);
+			$(window).load(function() {
+				$body.removeClass('is-loading');
 			});
 
 		// Fix: Placeholder polyfill.
