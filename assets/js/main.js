d6f27d7b26923c6d283f2569a55fa4f60ea99ce0
Rushikesh Kamalapurkar <rushikesh.kamalapurkar@okstate.edu>
2021-07-16 14:17:49 -0500

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
-				}, 500);
+				}, 1000);
 			});
 
 		// Fix: Placeholder polyfill.
